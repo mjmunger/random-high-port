56f4e7527103d6f3fd8522a70b65f8214abe8284
michael <mj@hph.io>
2018-06-04 09:33:01 -0400

    Updated for Python3 and PEP8

--- get-random-high-port.py
+++ get-random-high-port.py
@@ -1,42 +1,44 @@
-import requests, re, sys,os, random
+#!/usr/bin/env python3
+import requests, re, sys, os, random
 
-class portRange:
-	lower = None
-	upper = None
-	ports = None
 
-	def __init__(self,lower,upper):
-		self.lower = lower
-		self.upper = upper
-		self.ports = int(upper) - int(lower)
+class PortRange:
+    lower = None
+    upper = None
+    ports = None
 
-	def printRange(self):
-		print "%s - %s (%s ports)" % (self.lower,self.upper, self.ports)
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
+        self.ports = int(upper) - int(lower)
+
+    def print_range(self):
+        print("%s - %s (%s ports)" % (self.lower, self.upper, self.ports))
 
 
 limit = int(sys.argv[1]) if len(sys.argv) == 2 else 1
-print "Looking for %s contiguous unassigned ports..." % limit
-print "Downloading current IANA port list..."
+print("Looking for %s contiguous unassigned ports..." % limit)
+print("Downloading current IANA port list...")
 url = "http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt"
 pattern = "([0-9]{5})-([0-9]{5})"
 
 r = requests.get(url)
-print "OK"
-data = r.content
+print("OK")
+data = str(r.content)
 buff = iter(data.splitlines())
 
 ports = []
 for line in buff:
-	if "Unassigned" in line:
-		m = re.search(pattern,line)
-		if(m):
-			p = portRange(m.group(1), m.group(2))
-			if p.ports >= limit:
-				ports.append(p)
+    if "Unassigned" in line:
+        m = re.search(pattern, line)
+        if (m):
+            p = PortRange(m.group(1), m.group(2))
+            if p.ports >= limit:
+                ports.append(p)
 
 max = len(ports)
 min = 0
-pointer = random.randint(min,max)
-p=ports[pointer]
-print "Randomly selected candidate port range:"
-p.printRange()			
+pointer = random.randint(min, max)
+p = ports[pointer]
+print("Randomly selected candidate port range:")
+p.print_range()
